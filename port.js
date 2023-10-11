ef4540cc43bb1708fbf77bc7dac46f52544d0ba9
RoadKillerGR <147215540+RoadKillerGR@users.noreply.github.com>
2023-10-11 06:34:36 +0300

    Add files via upload

--- port.js
+++ port.js
@@ -23,17 +23,34 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+
+  // Handle keyboard arrow and escape key navigation
+  document.onkeydown = function (event) {
+    event = event || window.event;
+    if (event.keyCode == 37) {
+      plusSlides(-1); // Left arrow key
+    } else if (event.keyCode == 39) {
+      plusSlides(1); // Right arrow key
+    } else if (event.keyCode == 27) {
+      closeModal(); // Escape key
+    }
+  };
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace("active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
         
 // Top Button
